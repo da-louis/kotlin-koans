3cea0ae85ac13d55bf87f127ca77ada4d0c77ddd
da_louis <ruaces@gmail.com>
2018-12-21 22:08:02 +0900

    solve n27

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,16 +1,16 @@
 package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate>{
-    override operator fun compareTo(date: MyDate): Int {
+    override operator fun compareTo(other: MyDate): Int {
         return when{
-            year != date.year -> year - date.year
-            month != date.month -> month - date.month
-            else -> dayOfMonth - date.dayOfMonth
+            year != other.year -> year - other.year
+            month != other.month -> month - other.month
+            else -> dayOfMonth - other.dayOfMonth
         }
     }
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -19,7 +19,7 @@ enum class TimeInterval {
 }
 
 class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate> {
-    override operator fun contains(date: MyDate): Boolean {
-        return start <= date && date <= endInclusive
+    override operator fun contains(other: MyDate): Boolean {
+        return start <= other && other <= endInclusive
     }
 }
